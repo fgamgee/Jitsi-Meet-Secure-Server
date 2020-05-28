ad8b0e929a8d7c42ed998a084ea7df82289a889d
AWS <AWS@Kevins-MacBook-Air.local>
2020-05-28 10:41:43 -0400

    fix issues, add some ansible

--- Install.sh
+++ Install.sh
@@ -7,7 +7,7 @@ echo "Welcome to installation of private and secure Jitsi Meet server for Ubuntu
 # install firewall
 
 iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
+iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -s 127.0.0.0/8 -j DROP
@@ -26,16 +26,20 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+
+#Now save the iptables rules so they stay on reboot.
+apt-get install iptables-persistent
 netfilter-persistent save
+netfilter-persistent reload
 
 
 #Install Ansible
-#apt install -y ansible
+apt install -y ansible
 #Get Ansible playbook for CIS hardening -
 # From https://cloudsecuritylife.com/cis-ubuntu-script-to-automate-server-hardening/
-#sh -c "echo '- src: https://github.com/florianutz/Ubuntu1804-CIS.git' >> /etc/ansible/requirements.yml"
-#cd /etc/ansible/
-#ansible-galaxy install sudo iptables -P roles -r /etc/ansible/requirements.yml
+sh -c "echo '- src: https://github.com/florianutz/Ubuntu1804-CIS.git' >> /etc/ansible/requirements.yml"
+cd /etc/ansible/
+ansible-galaxy install sudo iptables -P roles -r /etc/ansible/requirements.yml
 # Make role - don't like this method.  Update needed.
 #sudo sh -c "cat > /etc/ansible/harden.yml <<EOF
 #- name: Harden Server
