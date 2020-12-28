015425cd906112f037fe5c5ca9ce366f6ce356ae
fgamgee <sgamgee@pm.me>
2020-12-28 12:37:50 -0500

    For Ubuntu20 with pre-hardened CIS

--- code/Install.sh
+++ code/Install.sh
@@ -5,6 +5,8 @@
 # One option to specify a branch -b other than 'master'
 # add others as we go...
 
+#Updating for Ubuntu2004.
+
 unset BRANCH STANDALONE
 
 BRANCH="master"
@@ -18,10 +20,10 @@ do
   esac
 done
 
-# Need to run these after login...  UPDATE to MASTER SOON!
+# Need to run these after login...  use BRANCH name, instead of master.
 #curl -o Install.sh https://raw.githubusercontent.com/fgamgee/Jitsi-Meet-Secure-Server/master/code/Install.sh
 #chmod +x Install.sh
-#sudo ./Install.sh
+#sudo ./Install.sh (argument BRANCH) (argument STANDALONE)
 
 #Set up for debugging, from https://wiki.bash-hackers.org/scripting/debuggingtips
 set -x
@@ -65,34 +67,13 @@ apt-get -y install iptables-persistent
 netfilter-persistent save
 netfilter-persistent reload
 
-#I just cannot get this way to work....
-# I think, because of this: https://stackoverflow.com/questions/17413526/nginx-missing-sites-available-directory#17415606
-# As the issue is sites-available directory is not there...  LOVE for someone else to fix it,
-# as this would be preferred.  For now, will stick to the ppa repository below.
-#Update the nginx packages
-#cat > /etc/apt/sources.list.d/nginx.list  <<EOF
-#deb https://nginx.org/packages/ubuntu/ bionic nginx
-#deb-src https://nginx.org/packages/ubuntu/ bionic nginx
-#EOF
-
-# Need to add the nginx key too.  See https://www.nginx.com/resources/wiki/start/topics/tutorials/install/
-# If this fails, see what the key is, and replace ABF5BD827BD9BF62
-# Err:6 https://nginx.org/packages/ubuntu bionic InRelease
-#  The following signatures couldn't be verified because the public key is not available: NO_PUBKEY ABF5BD827BD9BF62
-#
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
-# Try installing nginx before Jitsi
-#sudo apt-get update
-#sudo apt-get install nginx
+#From Quick Install
+apt -y install openjdk-8-jdk
 
 
-# This repository updated to 1.18.0, which breaks Jitsi-Meet.  This installs 1.14.0, not great....but works.
-# This works, but ppa is untrusted and only gets you to 1.16.1 nginx
-#add-apt-repository -y ppa:nginx/stable
-#apt-get -y update
 
 #add prosody repository and key.
-echo 'deb https://packages.prosody.im/debian bionic main' >> /etc/apt/sources.list.d/prosody.list
+echo 'deb https://packages.prosody.im/debian focal main' >> /etc/apt/sources.list.d/prosody.list
 wget -qO - https://prosody.im/files/prosody-debian-packages.key | apt-key add -
 apt-get -y update
 
@@ -104,8 +85,6 @@ apt-get -y install jitsi-meet
 
 #Let's Encrypt certificate - note, you will see this in the logs (at least nginx), because logging is
 # not yet disabled, but I think that is fine.
-# This method uses Let'sEncrypt from unstable, as that uses Python3
-curl -o /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh https://raw.githubusercontent.com/jitsi/jitsi-meet/master/resources/install-letsencrypt-cert.sh
 chmod +x /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 
@@ -132,82 +111,20 @@ ansible-playbook -v Jitsi_TLS_DH_config.yml
 # This is used to disable logging, comment out if you want logging.
 ansible-playbook -v Jitsi_no_logging.yml
 
+# Use pre-image CIS Level 1 instead of trying to depend on open-source Ansible CIS hardening.
 
-#Get Ansible playbook for CIS hardening -
-# From https://cloudsecuritylife.com/cis-ubuntu-script-to-automate-server-hardening/
-sh -c "echo '- src: https://github.com/fgamgee/CIS-florianutz_stable.git' >> /etc/ansible/requirements.yml"
 
-ansible-galaxy install -p roles -r /etc/ansible/requirements.yml
-# Make role - don't like this method.  Update needed, maybe move to an ansible file?
-cat > /etc/ansible/harden.yml <<EOF
-- name: Harden Server
-  hosts: localhost
-  connection: local
-  become: yes
-
-  roles:
-    - CIS-florianutz_stable
-EOF
 
 # X Windows is not installed, as can be checked with  dpkg -l xserver-xorg*
 # but Ansible xwindows task removes x11 which Jitsi does need
 # Needs to have dummy telnet package installed and telnet removed - but Coturn
-# has a dependency of telnet (but it is only used for debugging).
+# has a dependency of telnet (but it is only used for debugging).  Install for now.
+apt-get -y install telnet
+#
+
 # I currently use iptables.
 # Rule 4.3 is logrotate, which prosody does not like...  get rid of logging is
 # a privacy goal of logrotate is not relevant.
-cat > /etc/ansible/roles/CIS-florianutz_stable/vars/main.yml  <<EOF
----
-# vars file for Ubuntu1804-CIS
-ubuntu1804cis_xwindows_required: true
-ubuntu1804cis_telnet_required: true
-ubuntu1804cis_firewall: iptables
-#logrotate seems to break coturn.  Probably need to add simple-log to turnserver.conf
-# to use logrotate (not tested).
-ubuntu1804cis_rule_4_3: false
-# Below is to disable Auditing and logging, to maintain privacy -
-# no record on system if compromised - but no record of compromise either....
-# There is a privacy/security tradeoff for the user to consider.  If you want
-# logging and auditiing, delete the line below
-ubuntu1804cis_section4: false
-
-EOF
-
-# For standalone, no SSH
-# note, not as readable, as <<- did not seem to work for me, not sure why...
-# https://stackoverflow.com/questions/2500436/how-does-cat-eof-work-in-bash
-if [ "$STANDALONE" = true ]; then
-cat >> /etc/ansible/roles/CIS-florianutz_stable/vars/main.yml << EOF
-# Below here, is SSH rules, which are not on the standalone server (requires physical
-# access with keyboard, mouse and monitor, no remote administration.)
-ubuntu1804cis_rule_5_2_1: false
-ubuntu1804cis_rule_5_2_2: false
-ubuntu1804cis_rule_5_2_3: false
-ubuntu1804cis_rule_5_2_4: false
-ubuntu1804cis_rule_5_2_5: false
-ubuntu1804cis_rule_5_2_6: false
-ubuntu1804cis_rule_5_2_7: false
-ubuntu1804cis_rule_5_2_8: false
-ubuntu1804cis_rule_5_2_9: false
-ubuntu1804cis_rule_5_2_10: false
-ubuntu1804cis_rule_5_2_11: false
-ubuntu1804cis_rule_5_2_12: false
-ubuntu1804cis_rule_5_2_13: false
-ubuntu1804cis_rule_5_2_14: false
-ubuntu1804cis_rule_5_2_15: false
-ubuntu1804cis_rule_5_2_16: false
-ubuntu1804cis_rule_5_2_17: false
-ubuntu1804cis_rule_5_2_18: false
-ubuntu1804cis_rule_5_2_19: false
-ubuntu1804cis_rule_5_2_20: false
-ubuntu1804cis_rule_5_2_21: false
-ubuntu1804cis_rule_5_2_22: false
-ubuntu1804cis_rule_5_2_23: false
-EOF
-fi
-
-cd /etc/ansible/
-ansible-playbook /etc/ansible/harden.yml
 
 set +x
 
@@ -306,9 +223,12 @@ systemctl start jicofo.service
 
 # Stop and disable system logging - Comment out these lines if you want to keep logging.
 #https://stackoverflow.com/questions/17358499/linux-how-to-disable-all-log#32553762
-printf "Stopping and Disabling logging..."
-systemctl stop rsyslog.service
-systemctl disable rsyslog.service
+
+#TBD - turn back on.
+
+#printf "Stopping and Disabling logging..."
+#systemctl stop rsyslog.service
+#systemctl disable rsyslog.service
 
 
 printf "Installation is complete! You can test Jitsi now by starting a meeting.\n"
